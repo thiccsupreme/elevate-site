d6b73bc31462bcfe8a8121eea4fd267628945736
thiccsupreme <57068146+thiccsupreme@users.noreply.github.com>
2021-01-15 23:49:01 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+$(document).ready(async function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $(".navbar").addClass("sticky");
+        } else {
+            $(".navbar").removeClass("sticky");
+        }
+        if (this.scrollY > 500) {
+            $(".scroll-up-btn").addClass("show");
+        } else {
+            $(".scroll-up-btn").removeClass("show");
+        }
+    });
+
+    $(".scroll-up-btn").click(function () {
+        $("html").animate({ scrollTop: 0 })
+    })
+
+
+
+    $(".menu-btn").click(function () {
+        $(".navbar .menu").toggleClass("active");
+        $(".menu-btn i").toggleClass("active");
+    })
+})
+
+
+// var typed = new Typed(".typing", {
+//     strings: ["Developer.", "Sneaker Botter.", "Reseller.", "Designer.", "YouTuber."],
+//     typeSpeed: 90,
+//     backSpeed: 60,
+//     loop: true
+// })
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
