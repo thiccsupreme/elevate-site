278b81f805c2f382cb677a61729b9ebbcf4a7009
thiccsupreme <57068146+thiccsupreme@users.noreply.github.com>
2021-01-19 16:09:13 -0500

    Cleaned up and formatted the code!

--- script.js
+++ script.js
@@ -1,47 +1,35 @@
 $(document).ready(async function () {
-    $(window).scroll(function () {
-        if (this.scrollY > 20) {
-            $(".navbar").addClass("sticky");
-        } else {
-            $(".navbar").removeClass("sticky");
-        }
-        if (this.scrollY > 500) {
-            $(".scroll-up-btn").addClass("show");
-        } else {
-            $(".scroll-up-btn").removeClass("show");
-        }
-    });
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
 
-    $(".scroll-up-btn").click(function () {
-        $("html").animate({ scrollTop: 0 })
-    })
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 })
+  })
 
 
 
-    $(".menu-btn").click(function () {
-        $(".navbar .menu").toggleClass("active");
-        $(".menu-btn i").toggleClass("active");
-    })
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  })
 })
-
-
-// var typed = new Typed(".typing", {
-//     strings: ["Developer.", "Sneaker Botter.", "Reseller.", "Designer.", "YouTuber."],
-//     typeSpeed: 90,
-//     backSpeed: 60,
-//     loop: true
-// })
 
 var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
